f57b73744a75bcf2da5c14e3ff18fd059bc69edc
Amit Kumar <amiit282@gmail.com>
2019-09-13 04:10:04 +0530

    Commit

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const helmet = require('helmet')
 
 app.use(helmet.contentSecurityPolicy({
   directives: {
-    defaultSrc: ["'*'"],
-    imgSrc: ["'*'"],
-    fontSrc: ["'*'"],
-    styleSrc: ["'*'", 'maxcdn.bootstrapcdn.com']
+    "default-src": ["'*'"],
+    "img-src": ["'*'"],
+    "font-src": ["'*'"],
+    "style-src": ["'*'", 'maxcdn.bootstrapcdn.com']
   }
 }))
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080)
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
