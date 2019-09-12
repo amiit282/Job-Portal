316214d35b7bcb2fb42243f48168cd237c057d0d
Amit Kumar <amiit282@gmail.com>
2019-09-13 04:14:52 +0530

    Commit

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -9,9 +9,10 @@ import { HttpClient } from '@angular/common/http';
 export class HomepageComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.http.get('https://jobsqared.herokuapp.com/jobs').subscribe(data => {
-      console.log(data);
-    });
-  }
+  ngOnInit() {}
+  // ngOnInit() {
+  //   this.http.get('https://jobsqared.herokuapp.com/jobs').subscribe(data => {
+  //     console.log(data);
+  //   });
+  // }
 }
