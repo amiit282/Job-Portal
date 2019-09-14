d10857b211c4a16a7df4a18725cbce2d60944a51
Amit Kumar <amiit282@gmail.com>
2019-09-14 14:40:48 +0530

    server.js

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const favicon = require('serve-favicon')
 
 
 
-app.use(express.static(path.join(__dirname, 'dist', 'jobportal')));
+app.use(express.static(path.join(__dirname, 'dist', 'jobPortal')));
 
 app.get('*', function (req, res) {
   console.log('Redirect method');
-  res.sendFile(path.join(__dirname, 'dist', 'jobportal', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'jobPortal', 'index.html'));
 });
 app.listen(process.env.PORT || 8080)
