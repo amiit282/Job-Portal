edab44b796e7b12d6347b8d7eac2dde9d1202fc7
Amit Kumar <amiit282@gmail.com>
2019-09-14 15:01:12 +0530

    server.js

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -9,7 +9,6 @@ import { HttpClient } from '@angular/common/http';
 export class HomepageComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
   ngOnInit() {
     this.http.get('https://jobsqared.herokuapp.com/jobs').subscribe(data => {
       console.log(data);
