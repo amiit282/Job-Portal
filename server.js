bb4b9b0f6a889e64b3c12278190899a81c6a53f4
Amit Kumar <amiit282@gmail.com>
2019-09-14 13:42:36 +0530

    server.js

--- server.js
+++ server.js
@@ -12,5 +12,4 @@ app.get('/*', function (req, res) {
   console.log('Redirect method');
   res.sendFile(path.join(__dirname, './dist/jobportal/index.html'));
 });
-app.use(favicon(path.join(__dirname, './dist/jobportal', 'favicon.ico')))
 app.listen(process.env.PORT || 8080)
